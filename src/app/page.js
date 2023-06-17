329e8ee98f127584560918c075def89a56dde384
Roque Lagrotta <roque@rocketpowerstudios.com>
2023-06-17 17:06:21 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import LoginAsync from './LoginForm'
+import styles from "./page.module.css";
+import { auth } from "./firebase";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebase from "./firebase";
+import {useAuthState} from "react-firebase-hooks/"
+import {useEffect} from "react"
 
 export default function Home() {
+  const googleAuth = new GoogleAuthProvider();
+
+  const login = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleAuth);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-      
-      <div>
-      <h1>Mi aplicación</h1>
-      <LoginAsync />
+    <div className={styles.container}>
+      <h1>Login with Next.js Google Auth</h1>
+      <button onClick={login}>Login</button>
     </div>
-  )
+  );
 }
